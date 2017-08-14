99da5b4296ce00365965f8d65a0a2c49679fa1e8
Sebastian Baumann <bauman21@hm.edu>
2017-08-14 17:53:25 +0200

    Updating .gitignore file. Correcting and translating comments into english.

--- src/Time.java
+++ src/Time.java
@@ -1,66 +1,74 @@
-/*
- * Organisation: Hochschule Muenchen, Fakultaet 07
- * Project: Praktikum Softwareentwicklung 1 Frau Prof. Dr. Hammerschall, WS 2016/2017
- * Author: Sebastian Baumann, bauman21@hm.edu
+/**
+ * Organisation: University of applied sciences Munich, faculty 07
+ * Project: lab to class software developement 1 Mrs. Prof. Dr. Hammerschall, WS 2016/2017
+ * Author: Sebastian Baumann, sebastian_baumann@gmx.net
  * Study group: IF3A
  * Date: 27. November 2016
- * Purpose: Loesung Praktikumseinheit 05: Time
- * Software: Oracle Java SE JDK 1.8.0_101, Windows 10 x86_64
- * Hardware: Intel Core i5-6300 @ 2.40 GHz 2.50 GHz, 2 Cores, 4096 MB RAM
+ * Purpose: solution to lab Time
  */
-
-/*
- * Hinweise:
- * add:     komplexeste Methode, Loesung mit maximal 9 Zeilen
- * diff:    Pruefung auf Null, maximal 4 Zeilen (inkl. korrekter Klammerung)
- * isEqual: Pruefung auf Null, maximal 4 Zeilen (inkl. korrekter Klammerung)
- * ae:      Eine einzige Zeile im Methodenrumpf
- */
-
 public class Time {
 
-    // Deklaration und Intitialisierung der benoetigten Konstanten -------------------------
-    private static final int SECONDS_A_DAY = 86400;                         // 86400 Sekunden hat ein voller Tag
-    private static final int SECONDS_AN_HOUR = 3600;                        // 3600 Sekunden hat eine Stunde
-    private static final int SECONDS_A_MINUTE = 60;                         // 60 Sekunden hat eine Minute
+    /**
+     * Private constant to store the amounts of seconds of a whole day.
+     */
+    private static final int SECONDS_A_DAY = 86400;
+
+    /**
+     * Private constant to store the amounts of seconds of a whole hour.
+     */
+    private static final int SECONDS_AN_HOUR = 3600;
+
+    /**
+     * Private constant to store the amounts of seconds of a whole minute.
+     */
+    private static final int SECONDS_A_MINUTE = 60;
 
 
-    // Deklaration der benoetigten Objektvariablen -----------------------------------------
+    /**
+     * Private variable to store the amounts of seconds of this time object.
+     */
     private int timeInSeconds;
 
 
-    // Default-Konstruktor, der das Objekt mit 00:00:00 initialisiert.
-    // Benutzt Time(int seconds) {}
+    /**
+     * Constructor which needs no parameters.
+     */
     Time() {
 
         this(0);
 
     }
 
-    // Konstruktor, der Sekunden akzeptiert. Stunden und Minuten werden auf 0 gesetzt.
-    // Benutzt Time(int hours, int minutes, int seconds) {}
+    /**
+     * Constructor which needs the amount of seconds of this time object as a parameter.
+     */
     Time(int seconds) {
 
         this(0, 0, seconds);
 
     }
 
-    // Konstruktor, der Stunden und Minuten akzeptiert, Sekunden werden automatisch auf 0 gesetzt
-    // Benutzt Time(int hours, int minutes, int seconds) {}
+    /**
+     * Constructor which needs the amount of hours and minutes as parameters.
+     */
     Time(int hours, int minutes) {
 
         this(hours, minutes, 0);
 
     }
 
-    // Konstruktoren ----------------------------------------------------------------------
+    /**
+     * Constructor which needs the amount of hours, minutes and seconds as parameters.
+     */
     Time(int hours, int minutes, int seconds) {
 
         this.timeInSeconds = seconds + (minutes * SECONDS_A_MINUTE) + (hours * SECONDS_AN_HOUR);
 
     }
 
-    // Copy-Konstruktor
+    /**
+     * Custom copy constructor.
+     */
     Time(Time obj) {
 
         this.timeInSeconds = obj.getTimeInSeconds();
@@ -68,50 +76,61 @@ public class Time {
     }
 
 
-    // Methoden ---------------------------------------------------------------------------
-    // Liefert die Anzahl an Sekunden im Intervall von 0 bis 59
+    /**
+     * Method to return the amount of seconds of this time object.
+     * @return - returns amount of seconds as an integer value.
+     */
     int getSeconds() {
 
         return ((this.getTimeInSeconds() % SECONDS_A_DAY) % SECONDS_AN_HOUR) % SECONDS_A_MINUTE;
 
     }
 
-    // Liefert die Anzahl an Minuten im Intervall 0 bis 59
+    /**
+     * Method to return the amount of minutes of this time object.
+     * @return - returns amount of minutes as an integer value.
+     */
     int getMinutes() {
 
         return ((this.getTimeInSeconds() % SECONDS_A_DAY) % SECONDS_AN_HOUR) / SECONDS_A_MINUTE;
 
     }
 
-    // Liefert die Anzahl an Stunden im Intervall von 0 bis 23
+    /**
+     * Method to return the amount of hours of this time object.
+     * @return - returns amount of hours as an integer value.
+     */
     int getHours() {
 
         return (this.getTimeInSeconds() % SECONDS_A_DAY) / SECONDS_AN_HOUR;
 
     }
 
-    // Liefert false zurueck, falls other == null oder falls dieses Objekt und other nicht uebereinstimmen
+    /**
+     * Method to determine if this time object is equal to another one.
+     * @param other - the other time object this time object has to be compared to.
+     * @return - returns true, if this and other are equal, and false if not.
+     */
     boolean isEqual(Time other) {
 
         return other != null && this.getTimeInSeconds() == other.getTimeInSeconds();
 
     }
 
-    // Liefert die gesamte gespeicherte Zeit in Sekunden zurueck
+    /**
+     * Method returns the stored time of this time object as seconds.
+     * @return - returns seconds as an integer value.
+     */
     int getTimeInSeconds() {
 
         return timeInSeconds;
 
     }
 
-    // Zaehlt zu der gespeicherten Zeit in Sekunden sec hinzu. Falls sec groesser oder gleich
-    // 0 ist oder die gespeicherte Zeit in Sekunden groesser ist als der Betrag von sec, dann wird sec einfach
-    // zur gesamten Zeit in Sekunden hinzugezaehlt/abgezogen, je nach Vorzeichen von sec. Ist der Betrag von
-    // sec aber groesser als die gespeicherte Zeit in Sekunden, so wird sicherlich eine Tagesgrenze rueckwaerts
-    // ueberschritten. Wird eine Tagesgrenze Rueckwaerts ueberschritten, so muss die restliche Zeit, die uebrig
-    // bleibt, wenn man von der gespeicherten Zeit in Sekunden sec abgezogen hat, von vollen 86400 Sekunden des
-    // Tages davor abgezogen werden. Ich teste in meiner Methode noch, ob sec gleich mehrere Tage von dem aktuellen
-    // Zeitpunkt abzieht. Deshalb das Modulo mit SECONDS_A_DAY.
+    /**
+     * Method to add time as seconds to this time object.
+     * @param sec - time as seconds which should be added to this time object.
+     */
     void add(int sec) {
 
         if (sec >= 0 || this.getTimeInSeconds() > Math.abs(sec)) {
@@ -127,7 +146,11 @@ public class Time {
         }
     }
 
-    // Liefert den Zeitunterschied von diesem Objekt zu Objekt other zurueck
+    /**
+     * Method to determine the difference of this time object to another time object 'other'.
+     * @param other - other time object this time object has to be compared to.
+     * @return - returns the difference of this time object to other time object as seconds as an inter value.
+     */
     int diff(Time other) {
 
         if (other == null) {
@@ -140,7 +163,10 @@ public class Time {
 
     }
 
-    // Liefert einen String in der Form hh:mm:ss an den Aufrufer zurueck
+    /**
+     * Method to generate a string representation of this time object.
+     * @return - returns a string representation of this time object.
+     */
     public String toString() {
 
         return String.format("%02d:%02d:%02d", getHours(), getMinutes(), getSeconds());
--- src/TimeInterval.java
+++ src/TimeInterval.java
@@ -1,29 +1,28 @@
-/*
- * Organisation: Hochschule Muenchen, Fakultaet 07
- * Project: Praktikum Softwareentwicklung 1 Frau Prof. Dr. Hammerschall, WS 2016/2017
- * Author: Sebastian Baumann, bauman21@hm.edu
+/**
+ * Organisation: University of applied sciences Munich, faculty 07
+ * Project: lab to class software developement 1 Mrs. Prof. Dr. Hammerschall, WS 2016/2017
+ * Author: Sebastian Baumann, sebastian_baumann@gmx.net
  * Study group: IF3A
  * Date: 27. November 2016
- * Purpose: Loesung Praktikumseinheit 05: TimeInterval
- * Software: Oracle Java SE JDK 1.8.0_101, Windows 10 x86_64
- * Hardware: Intel Core i5-6300 @ 2.40 GHz 2.50 GHz, 2 Cores, 4096 MB RAM
+ * Purpose: solution to lab TimeInterval
  */
-
-/*
- * Hinweise:
- * Konstruktoren:   In einem benoetigt man zwei Anweisungen
- * ae:              Eine einzige Anweisung im Methodenrumpf
- */
-
 public class TimeInterval {
 
-    // Deklaration der benoetigten Objektvariablen -----------------------------------------
-    private Time lower;     // Untergrenze des Intervalls
-    private int duration;   // Zeitraum des Intervalls als int-Wert
+    /**
+     * Private variable to store the lower border of this TimeInterval object.
+     */
+    private Time lower;
 
+    /**
+     * Private variable to store the difference from the start to the end of this TimeInterval object.
+     */
+    private int duration;
 
-    // Konstruktoren ----------------------------------------------------------------------
-    // Konstruktor, der die Startzeit als Time-Objekt und den Zeitraum erhaelt
+    /**
+     * Constructor which needs a start (as a Time object) and the length of this interval as seconds.
+     * @param lower - lower border of this TimeInterval object as a Time object.
+     * @param duration - the length of this TimeInterval object.
+     */
     public TimeInterval(Time lower, int duration) {
 
         this.lower = lower;
@@ -31,58 +30,84 @@ public class TimeInterval {
 
     }
 
-    // Konstruktor, der den Zeitraum als Wert erhaelt, wobei die Startzeit auf 00:00:00 gesetzt wird
+    /**
+     * Constructor which only needs the length of this TimeInterval object.
+     * The start for this TimeInterval object is automatically set to 00:00:00.
+     * @param duration - length of this TimeInterval in seconds.
+     */
     public TimeInterval(int duration) {
 
         this(new Time(), duration);
 
     }
 
-    // Copy-Konstruktor, der eine flache Kopie des uebergebenen Zeitintervalls macht
+    /**
+     * Constructor to make this TimeInterval object a copy of other TimeInterval object.
+     * @param other - other TimeInterval object this TimeInterval object should be a copy of.
+     */
     public TimeInterval(TimeInterval other) {
 
         this(other.getLower(), other.getDuration());
 
     }
 
-
-    // Methoden ---------------------------------------------------------------------------
-    // Liefert untere Grenze des Zeitintervalls zurueck
+    /**
+     * Method to return the start of this TimeInterval object as a Time object.
+     * @return - returns the start of this TimeInterval object as a Time object.
+     */
     Time getLower() {
 
         return this.lower;
 
     }
 
-    // Liefert obere Grenze des Zeitintervalls zurueck
+    /**
+     * Method to return the end of this TimeInterval object as a Time object.
+     * @return - returns the end of this TimeInterval object as a Time object.
+     */
     Time getUpper() {
 
         return new Time(this.getLower().getTimeInSeconds() + this.getDuration());
 
     }
 
-    // Liefert die Dauer des Zeitintervalls in Sekunden zurueck
+    /**
+     * Method to return the length of this TimeInterval object as seconds as an integer value.
+     * @return - returns the length of this TimeInterval object as seconds as an integer value.
+     */
     int getDuration() {
 
         return this.duration;
 
     }
 
-    // Verschiebt das Intervall um sec Sekunden
+    /**
+     * Method to move this TimeInterval object.
+     * @param sec - amount of seconds this TimeInterval object has to be moved.
+     */
     void move(int sec) {
 
         this.getLower().add(sec);
 
     }
 
-    // Erzeugt eine tiefe Kopie dieses Zeitintervalls und gibt diese zurueck
-    TimeInterval deepCopy() {
+    /**
+     * Method generated a deep copy of this TimeInterval object and returns a new TimeInterval object with the values of this one.
+     * @return - returns a new TimeInterval object as a deep copy of this TimeInterval object.
+     */
+    public TimeInterval deepCopy() {
 
         return new TimeInterval(new Time(this.getLower()), this.getDuration());
 
     }
 
     // Liefert dieses Intervall in der Form [<Untere Grenze>,<Obere Grenze>] zurueck
+
+    /**
+     * Method generates a string representation of this TimeInterval object.
+     * The string representation looks like this: [hh:mm:ss,hh:mm:ss].
+     * @return - returns a string of this TimeInterval object.
+     */
     public String toString() {
 
         return String.format("[%s,%s]", this.getLower().toString(), this.getUpper().toString());
